5924b1f364b5296e6cda8ae4b1dbc9514f79f7df
ohdong9795 <ohdong9795@naver.com>
2024-05-30 21:13:29 +0900

    prettier 설정 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className='bg-black text-pink_E876EA w-[400px]'>랜딩 페이지 tailwind 테스트</div>;
+  return <div className="bg-black text-pink_E876EA w-[400px]">랜딩 페이지 tailwind 테스트</div>;
 }
