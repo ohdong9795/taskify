20afe44dc219ca1cf139975cae234f936b0a67eb
kuum97 <kuum97@icloud.com>
2024-06-01 19:03:53 +0900

    Feat: 여러 페이지를 라우팅으로 모달 구현

---
+++ app/modal-test/@modal/(.)new-dashboard/page.tsx
@@ -0,0 +1,12 @@
+import NewDashboard from '../../new-dashboard/page';
+import { Modal } from '../modal';
+
+function NewDashboardModal() {
+  return (
+    <Modal>
+      <NewDashboard />
+    </Modal>
+  );
+}
+
+export default NewDashboardModal;
--- app/modal-test/page.tsx
+++ app/modal-test/page.tsx
@@ -2,9 +2,12 @@ import Link from 'next/link';
 
 function TestPage() {
   return (
-    <div>
+    <div className="flex flex-col gap-2">
       <Link href="/modal-test/card" passHref>
         할 일 카드
+      </Link>
+      <Link href="/modal-test/new-dashboard" passHref>
+        대시보드 생성
       </Link>
     </div>
   );
