a8dce9c097c10468f99147cf8adaf2c1441c5840
xxs14c <xxs14c@dgu.ac.kr>
2024-06-03 18:41:57 +0900

    taskify 디렉토리 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
+'use client';
+
+import { ModalResetButton, ModalSubmitButton, PaginationButtons, ReturnButton, ModalProvider } from '../components/Button';
+
 export default function Home() {
-  return <div className="bg-black text-pink_E876EA w-[400px]">랜딩 페이지 tailwind 테스트</div>;
-}
+  return (
+    <ModalProvider>
+      <div className="bg-black text-pink_E876EA w-[400px] p-4 space-y-4">
+        <div>랜딩 페이지 Tailwind 테스트</div>
+        <ModalResetButton>Reset Button</ModalResetButton>
+        <ModalSubmitButton>Submit Button</ModalSubmitButton>
+        <PaginationButtons
+          allPage={10}
+          nowPage={1}
+          handleBackwardButtonClick={() => alert('Backward')}
+          handleForwardButtonClick={() => alert('Forward')}
+          isStart={false}
+          isEnd={false}
+        />
+        <ReturnButton />
+      </div>
+    </ModalProvider>
+  );
+} 
