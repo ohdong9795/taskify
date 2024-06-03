a41eaec01374dc32bfd83e00409d2e1a280f8ba5
hello <yup299@gmail.com>
2024-06-03 11:10:22 +0900

    fix

--- components/(SideBar)/SideBarUtil.ts
+++
@@ -1,9 +0,0 @@
-// import axiosInstance from '../../utils/axios';
-
-// export default async function SideBarUtil() {
-//   try {
-//     const { data } = await axiosInstance.get('dashboards');
-//   } catch (error) { 
-//     console.error(error);
-//   }
-// }
