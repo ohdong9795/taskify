f03f1b418723ccaf633650db81a119fe4bb206ab
hello <yup299@gmail.com>
2024-06-04 14:46:15 +0900

    fix : crlf to lf

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://sp-taskify-api.vercel.app/5-4/',
+  baseURL: 'https://sp-taskify-api.vercel.app/5-4',
   timeout: 1000,
 });
 
