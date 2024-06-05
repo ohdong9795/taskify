812c3ce67219a4034da9c0c3651c85d2178a80e3
ChoTD <jo7sy8@gmail.com>
2024-06-05 12:14:05 +0900

    FEAT: HEADER 수정

--- components/landing/Header.tsx
+++ components/landing/Header.tsx
@@ -1,4 +1,5 @@
 import MainImage from '@/public/images/MainImage.svg';
+import Link from 'next/link';
 
 export default function Description() {
   return (
@@ -9,9 +10,14 @@ export default function Description() {
         <p className=" text-violet_5534DA text-[76px] font-bold">Taskify</p>
       </div>
       <div className="text-white mt-[24px]">서비스의 메인 설명이 들어갑니다.</div>
-      <button type="submit" className="text-white mt-[66px] w-[280px] pt-[15px] pb-[14px] rounded-lg bg-violet_5534DA">
-        로그인하기
-      </button>
+      <Link href="/login">
+        <button
+          type="submit"
+          className="text-white mt-[66px] w-[280px] pt-[15px] pb-[14px] rounded-lg bg-violet_5534DA"
+        >
+          로그인하기
+        </button>
+      </Link>
     </header>
   );
 }
