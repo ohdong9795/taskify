c20780b2e3630a95ac6fc6da665d9cd2808ddf8f
ChoTD <jo7sy8@gmail.com>
2024-06-07 00:22:22 +0900

    FIX: 외부 링크 Link 컴포넌트로 수정

--- components/landing/Footer.tsx
+++ components/landing/Footer.tsx
@@ -1,21 +1,8 @@
-'use client';
-
 import { CiMail } from 'react-icons/ci';
 import { FaFacebookSquare, FaInstagram } from 'react-icons/fa';
+import Link from 'next/link';
 
 export default function Footer() {
-  const handleGmail = () => {
-    window.location.href = 'https://www.gmail.com';
-  };
-
-  const handleFacebook = () => {
-    window.location.href = 'https://www.facebook.com';
-  };
-
-  const handleInstagram = () => {
-    window.location.href = 'https://www.instagram.com';
-  };
-
   return (
     <div className=" bg-black flex flex-col lg:flex-row lg:justify-between items-center  w-full h-[300px] md:h-[100px]  text-gray_9FA6B2 px-[141px]">
       <div className="pb-[12px] md:pb-0">©codeit - 2023</div>
@@ -24,9 +11,15 @@ export default function Footer() {
         <div>FAQ</div>
       </div>
       <div className="flex gap-[20px] md:gap-[14px] text-white">
-        <CiMail onClick={handleGmail} className="w-[20px] h-[20px] cursor-pointer" />
-        <FaFacebookSquare onClick={handleFacebook} className="w-[20px] h-[20px] cursor-pointer" />
-        <FaInstagram onClick={handleInstagram} className="w-[20px] h-[20px] cursor-pointer" />
+        <Link href="https://www.gmail.com">
+          <CiMail className="w-[20px] h-[20px] cursor-pointer" />
+        </Link>
+        <Link href="https://www.facebook.com">
+          <FaFacebookSquare className="w-[20px] h-[20px] cursor-pointer" />
+        </Link>
+        <Link href="https://www.instagram.com">
+          <FaInstagram className="w-[20px] h-[20px] cursor-pointer" />
+        </Link>
       </div>
     </div>
   );
