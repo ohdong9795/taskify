8c372ce2c2b6b5d65755ad37d3a68a8b2f87d8b8
kuum97 <kuum97@icloud.com>
2024-06-08 00:16:27 +0900

    Feat: 나의 대시보드 페이지에서 쿠키헤더로 데이터 페칭 성공

---
+++ app/mydashboard/page.tsx
@@ -0,0 +1,25 @@
+import DashboardList from '@/components/DashboardList';
+import { DashboardData } from '@/types/user/dashboard';
+import { cookies } from 'next/headers';
+
+const URL = 'https://sp-taskify-api.vercel.app/5-4/dashboards?navigationMethod=infiniteScroll&page=1&size=10';
+
+async function getDashboards() {
+  const cookieStore = cookies();
+  const token = cookieStore.get('token');
+  const response = await fetch(URL, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token?.value}`,
+    },
+  });
+  const json = await response.json();
+  return json;
+}
+
+async function MyDashboardPage() {
+  const dashboards: DashboardData = await getDashboards();
+  return <DashboardList dashboardData={dashboards} />;
+}
+
+export default MyDashboardPage;
