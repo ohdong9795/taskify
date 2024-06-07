d5318ddfebfb7223983b2c3dd3145f17e3ad90e0
hello <yup299@gmail.com>
2024-06-07 14:32:48 +0900

    feat : sidebar 기능 추가

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import SideBarList from './SideBarList';
 import Logo from '../../public/images/sideMenuLogo.svg';
 
@@ -6,7 +5,7 @@ function SideMenu() {
   return (
     <div className="flex flex-col h-screen w-[300px] border-r-[1px] border-gray-D9D9D9">
       <div className="mt-5 ml-6">
-        <Image src={Logo} alt="로고" />
+        <Logo />
       </div>
       <SideBarList />
     </div>
