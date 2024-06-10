d69491d69bf50a1c9d1eee2f53cd95747c78b532
kuum97 <kuum97@icloud.com>
2024-06-10 22:07:37 +0900

    Feat: 초대필드 타입 명시

--- types/user/dashboard.ts
+++ types/user/dashboard.ts
@@ -1,15 +1,42 @@
+export interface Dashboard {
+  id: number;
+  title: string;
+  color: string;
+  userId: number;
+  createdAt: string;
+  updatedAt: string;
+  createdByMe: boolean;
+}
+
 export interface DashboardData {
-  dashboards: [
-    {
-      id: number;
-      title: string;
-      color: string;
-      userId: number;
-      createdAt: string;
-      updatedAt: string;
-      createdByMe: boolean;
-    },
-  ];
+  dashboards: Dashboard[];
   totalCount: number;
   cursorId: number | null;
+}
+
+export interface Invitation {
+  id: number;
+  inviter: {
+    nickname: string;
+    email: string;
+    id: number;
+  };
+  teamId: string;
+  dashboard: {
+    title: string;
+    id: number;
+  };
+  invitee: {
+    nickname: string;
+    email: string;
+    id: number;
+  };
+  inviteAccepted: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface InvitationData {
+  invitations: Invitation[];
+  cursorId: number;
 }
