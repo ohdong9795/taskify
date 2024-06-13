2099783fe98b689ecd240fc90541bc0afe1017e8
hello <yup299@gmail.com>
2024-06-13 14:23:06 +0900

    fix : 서비스폴더점검

--- components/myPage/Profile/BackIcon.tsx
+++ components/myPage/Profile/BackIcon.tsx
@@ -1,16 +1,21 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { IoIosArrowBack } from 'react-icons/io';
 
 export default function BackIcon() {
+  const router = useRouter();
+
   const handleButtonClick = () => {
     router.back();
   };
 
   return (
-    <div className="flex ml-[20px] mt-[20px]">
-      <IoIosArrowBack className="w-[20px] h-[20px]" />
-      <span className="text-[16px] font-medium text-black_333236">돌아가기</span>
-    </div>
+    <button onClick={handleButtonClick}>
+      <div className="flex ml-[20px] mt-[20px]">
+        <IoIosArrowBack className="w-[20px] h-[20px]" />
+        <span className="text-[16px] font-medium text-black_333236">돌아가기</span>
+      </div>
+    </button>
   );
 }
