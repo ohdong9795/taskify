8a319a9811ca86fd32e8d00651f6ab0048154a51
ohdong9795 <ohdong9795@naver.com>
2024-06-13 17:05:05 +0900

    feat: sidebar

--- components/SideBar/SideBarItem.tsx
+++ components/SideBar/SideBarItem.tsx
@@ -1,32 +1,49 @@
 'use client';
 
-import classnames from 'classnames';
-import { dashboardsStore } from '../../stores/dashboardsStore';
+import { useState } from 'react';
+import { Dashboard } from '@/stores/dashboardsStore';
+import { colors } from '@/constants/dashboardColors';
+import Link from 'next/link';
 import CrownLogo from '../../public/images/DashboardCrown.svg';
 import DashboardLogo from '../../public/images/DashboardIcon.svg';
 
-export default function SideBarItem() {
-  const dashboards = dashboardsStore((state) => state.dashboards);
+const ITEMS_PER_PAGE = 18;
+
+export default function SideBarItem({ dashboards }: { dashboards: Dashboard[] | null }) {
+  const [page, setPage] = useState(1);
+
+  const startIndex = (page - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  const paginatedDashboards = dashboards?.slice(startIndex, endIndex);
 
   return (
     <>
-      {dashboards.slice(0, 15).map((dashboard) => {
-        const colorClass = classnames({
-          'text-green-500': dashboard.color === 'green',
-          'text-purple-500': dashboard.color === 'purple',
-          'text-orange-500': dashboard.color === 'orange',
-          'text-blue-500': dashboard.color === 'blue',
-          'text-pink-500': dashboard.color === 'pink',
-        });
+      <div className="flex flex-col gap-3">
+        {paginatedDashboards?.map((dashboard) => {
+          const colorClass = `text-${colors[dashboard.color]}`;
 
-        return (
-          <div key={dashboard.id}>
-            <DashboardLogo className={colorClass} />
-            <p className={colorClass}>{dashboard.title}</p>
-            {dashboard.createdByMe && <CrownLogo className={colorClass} />}
-          </div>
-        );
-      })}
+          return (
+            <Link href={`/dashboard/${dashboard.id}`} key={dashboard.id}>
+              <div className="flex items-center gap-1">
+                <DashboardLogo className={colorClass} />
+                <p className={colorClass}>{dashboard.title}</p>
+                {dashboard.createdByMe && <CrownLogo className={colorClass} />}
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+      <div>
+        <button onClick={() => setPage((prevPage) => prevPage - 1)} disabled={page === 1}>
+          {'<-'}
+        </button>
+        <button
+          onClick={() => setPage((prevPage) => prevPage + 1)}
+          disabled={page >= Math.ceil((dashboards ? dashboards.length : 0) / ITEMS_PER_PAGE)}
+        >
+          {'->'}
+        </button>
+      </div>
     </>
   );
 }
---
+++ constants/dashboardColors.ts
@@ -0,0 +1,15 @@
+export const colorsArray = [
+  { name: 'green-500', code: '#22c55e' },
+  { name: 'purple-600', code: '#9333ea' },
+  { name: 'orange-500', code: '#f97316' },
+  { name: 'sky-300', code: '#7dd3fc' },
+  { name: 'pink-500', code: '#ec4899' },
+];
+
+export const colors: { [key: string]: string } = {
+  '#22c55e': 'green-500',
+  '#9333ea': 'purple-600',
+  '#f97316': 'orange-500',
+  '#7dd3fc': 'sky-300',
+  '#ec4899': 'pink-500',
+};
