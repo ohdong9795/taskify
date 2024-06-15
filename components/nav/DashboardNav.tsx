f18a86e84d3f4e02178d77534b2bebba9f735b42
kuum97 <kuum97@icloud.com>
2024-06-15 15:16:09 +0900

    Refactor: 대시보드 네비게이션 리팩토링

--- components/nav/DashboardNav.tsx
+++ components/nav/DashboardNav.tsx
@@ -2,170 +2,134 @@
 
 // '/mydashboard', 'mypage'에서 사용하는 nav입니다.
 
+import useModal from '@/hooks/useModal';
 import { IoMdSettings } from 'react-icons/io';
 import { MdOutlineAddBox } from 'react-icons/md';
-import Vector from '@/public/images/Vector.svg';
+import { Member } from '@/types/user/members';
 import UserImage from '@/public/images/UserImage.svg';
 import { usePathname, useRouter } from 'next/navigation';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import useAuthStore from '@/stores/authStore';
 import Link from 'next/link';
-import axios from 'axios';
 import useDataStore from '@/stores/dataStore';
 import Image from 'next/image';
 import { getMembers } from '@/services/client/members';
 import Dropdown, { DropdownHandle } from '../common/Dropdwon';
-import Modal, { ModalHandles } from '../Modal';
+import Modal from '../Modal';
 import Button from './Button';
 import InviteForm from '../Modal/views/InviteForm';
 
-interface MembersType {
-  members: [
-    {
-      id: number;
-      userId: number;
-      email: string;
-      nickname: string;
-      profileImageUrl: string;
-      createdAt: string;
-      updatedAt: string;
-      isOwner: boolean;
-    },
-  ];
-  totalCount: number;
-}
-
 export default function DashBoardNav() {
-  const [title, setTitle] = useState('');
+  const { dashboards } = useDataStore();
+  const { setLogout, user } = useAuthStore();
+  const router = useRouter();
+  const pathName = usePathname();
+  const { modalRef, handleOpenModal } = useModal();
   const dropdownRef = useRef<DropdownHandle>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
-  const modalRef = useRef<ModalHandles>(null);
-  const pathName = usePathname();
   const dashboardId = parseInt(pathName.slice(11, 15), 10);
-  const router = useRouter();
-  const { dashboards } = useDataStore();
-
   const isDashboard = pathName.includes(`/dashboard/${dashboardId}`);
   const isMyPage = pathName === '/mypage';
+  const currentDashboard = dashboards?.find((dashboard) => dashboard.id === dashboardId);
+  const [membersData, setMembersData] = useState<Member[] | null>(null);
 
-  const findDashboard = dashboards?.find((dashboard) => dashboard.id === dashboardId);
-
-  const { clearToken, clearUser, user } = useAuthStore();
-  const handleLogout = () => {
-    clearToken();
-    clearUser();
-    localStorage.clear();
-    axios.get('/api/logout');
-    router.push('/');
-  };
-
-  const [members, setMembers] = useState<MembersType | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      const result = await getMembers({ dashboardId });
-      setMembers(result);
-    })();
+  const fetchMembers = useCallback(async () => {
+    const result = await getMembers({ dashboardId });
+    const { members } = result;
+    setMembersData(members);
   }, [dashboardId]);
 
   useEffect(() => {
     if (isDashboard) {
-      setTitle(findDashboard?.title ?? '');
-    } else if (isMyPage) {
-      setTitle('계정관리');
-    } else {
-      setTitle('내 대시보드');
+      fetchMembers();
     }
-  }, [pathName, isDashboard, isMyPage, findDashboard]);
+  }, [isDashboard, fetchMembers]);
 
   const toggleDropdown = () => {
     dropdownRef.current?.toggle();
   };
 
-  const handleOpenModal = () => {
-    modalRef.current?.open();
+  const handleLogout = () => {
+    setLogout();
+    router.push('/');
   };
 
   return (
-    <nav className="w-full h-[70px] bg-white shrink-0 flex self-end justify-between items-center px-[40px]">
-      <div className="font-bold hidden t:block">{title}</div>
+    <nav className="w-full h-[70px] bg-white shrink-0 flex self-end justify-between items-center px-[40px] border-b border-gray_D9D9D9">
+      <div className="hidden font-bold t:block">
+        {(isMyPage && '계정관리') || (isDashboard && currentDashboard?.title) || '나의 대시보드'}
+      </div>
       <div className="flex flex-row items-center gap-[40px]">
-        {isDashboard ? (
-          <div className="flex flex-row items-center gap-[16px]">
-            <Link
-              href={`/dashboard/${dashboardId}/edit`}
-              className="flex items-center gap-[8px] rounded-md border border-gray_D9 py-[11px] px-[16px] text-gray_787486"
-            >
-              <IoMdSettings className="hidden t:flex" />
-              관리
-            </Link>
-            <Button text="초대하기" ref={buttonRef} handleClick={handleOpenModal}>
-              <MdOutlineAddBox className="hidden t:flex" />
-            </Button>
-          </div>
-        ) : null}
-        <div className="flex flex-row items-center gap-[32px]">
-          {isDashboard ? (
-            <>
-              <div className="flex items-center">
-                {members ? (
-                  members.members
-                    .filter((member) => member.nickname !== user?.nickname)
-                    .map((member, index) => (
-                      <div key={member.id} className={`relative ${index > 0 ? '-ml-3' : ''}`}>
-                        {member.profileImageUrl ? (
-                          <Image
-                            src={member.profileImageUrl}
-                            alt={member.nickname}
-                            width={38}
-                            height={38}
-                            className=" rounded-full border-white border-2"
-                          />
-                        ) : (
-                          <div className="w-[40px] h-[40px] flex items-center justify-center rounded-full bg-gray-200 border-2 border-white text-gray-700 font-bold ">
-                            {member.nickname.charAt(0)}
-                          </div>
-                        )}
-                      </div>
-                    ))
-                ) : (
-                  <UserImage />
-                )}
-              </div>
-              <Vector />
-            </>
-          ) : null}
-          <div className="flex flex-row items-center gap-[12px]">
-            <div>
-              {user?.profileImageUrl ? (
-                <Image
-                  src={user?.profileImageUrl}
-                  width={38}
-                  height={38}
-                  alt="사용자 이미지"
-                  className="rounded-full "
-                  onClick={toggleDropdown}
-                />
+        {isDashboard && (
+          <>
+            <div className="flex flex-row items-center gap-[16px]">
+              <Link
+                href={`/dashboard/${dashboardId}/edit`}
+                className="flex items-center gap-[8px] rounded-md border border-gray_D9 py-[11px] px-[16px] text-gray_787486"
+              >
+                <IoMdSettings className="hidden t:flex" />
+                관리
+              </Link>
+              <Button text="초대하기" ref={buttonRef} handleClick={handleOpenModal}>
+                <MdOutlineAddBox className="hidden t:flex" />
+              </Button>
+            </div>
+            <div className="flex items-center">
+              {membersData ? (
+                membersData
+                  .filter((member) => member.nickname !== user?.nickname)
+                  .map((member, index) => (
+                    <div key={member.id} className={`relative ${index > 0 ? '-ml-3' : ''}`}>
+                      {member.profileImageUrl ? (
+                        <Image
+                          src={member.profileImageUrl}
+                          alt={member.nickname}
+                          width={38}
+                          height={38}
+                          className="border-2 border-white rounded-full "
+                        />
+                      ) : (
+                        <div className="w-[40px] h-[40px] flex items-center justify-center rounded-full bg-gray-200 border-2 border-white text-gray-700 font-bold ">
+                          {member.nickname.charAt(0)}
+                        </div>
+                      )}
+                    </div>
+                  ))
               ) : (
-                <UserImage onClick={toggleDropdown} />
+                <UserImage />
               )}
             </div>
-
-            <button className="hidden t:flex" onClick={toggleDropdown}>
-              {user?.nickname}
-            </button>
-            <Dropdown ref={dropdownRef}>
-              <Link href="/mydashboard" className="block w-full px-4 py-2 text-left hover:bg-gray-100">
-                내 대시보드
-              </Link>
-              <Link href="/mypage" className="block w-full px-4 py-2 text-left hover:bg-gray-100">
-                내정보
-              </Link>
-              <button onClick={handleLogout} className="block w-full px-4 py-2 text-left hover:bg-gray-100">
-                로그아웃
-              </button>
-            </Dropdown>
+          </>
+        )}
+        <div className="flex flex-row items-center gap-[12px] border-l px-8 border-gray_D9D9D9">
+          <div>
+            {user?.profileImageUrl ? (
+              <Image
+                src={user?.profileImageUrl}
+                width={38}
+                height={38}
+                alt="사용자 이미지"
+                className="rounded-full "
+                onClick={toggleDropdown}
+              />
+            ) : (
+              <UserImage onClick={toggleDropdown} />
+            )}
           </div>
+          <button className="hidden t:flex" onClick={toggleDropdown}>
+            {user?.nickname}
+          </button>
+          <Dropdown ref={dropdownRef}>
+            <Link href="/mydashboard" className="block w-full px-4 py-2 text-left hover:bg-gray-100">
+              내 대시보드
+            </Link>
+            <Link href="/mypage" className="block w-full px-4 py-2 text-left hover:bg-gray-100">
+              내정보
+            </Link>
+            <button onClick={handleLogout} className="block w-full px-4 py-2 text-left hover:bg-gray-100">
+              로그아웃
+            </button>
+          </Dropdown>
         </div>
       </div>
       <Modal ref={modalRef}>
