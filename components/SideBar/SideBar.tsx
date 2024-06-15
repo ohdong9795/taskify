5d049b184f4629e944e350553faf5b1bce4afadd
ChoTD <jo7sy8@gmail.com>
2024-06-15 22:28:20 +0900

    사이드 바 반응형 디자인 추가

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -1,11 +1,13 @@
+import LogoImage from '@/public/images/LogoImage.svg';
+import Taskify from '@/public/images/Taskify.svg';
 import SideBarList from './SideBarList';
-import Logo from '../../public/images/sideMenuLogo.svg';
 
 function SideBar() {
   return (
-    <div className="flex flex-col h-auto w-[300px] border-r-[1px] border-gray-D9D9D9 overscroll-y-none">
-      <div className="mt-5 ml-6">
-        <Logo />
+    <div className="flex flex-col h-auto w-[67px] t:w-[160px] p:w-[300px] border-r-[1px] border-gray-D9D9D9 overscroll-y-none">
+      <div className="mt-5 ml-6 flex items-center">
+        <LogoImage className="w-[29px] h-[33px] text-violet_5534DA " />
+        <Taskify className="w-[80px] h-[22px] text-violet_5534DA hidden t:block" />
       </div>
       <SideBarList />
     </div>
